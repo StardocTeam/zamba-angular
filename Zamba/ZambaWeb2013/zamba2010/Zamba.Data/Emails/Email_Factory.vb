fdf884df01919f4d33684ed1cc7bb197ae7744a3
EmilianoAlvarezST <69012148+EmilianoAlvarezST@users.noreply.github.com>
2023-02-24 16:26:00 -0300

    EA-Zamba: Ae incluyo el formulario 'NotificacionDemandaReadOnlyDropZone.html' al proyecto

--- Zamba/ZambaWeb2013/zamba2010/Zamba.Data/Emails/Email_Factory.vb
+++ Zamba/ZambaWeb2013/zamba2010/Zamba.Data/Emails/Email_Factory.vb
@@ -117,6 +117,7 @@ Public NotInheritable Class Email_Factory
         End Try
     End Sub
 
+
     Public Shared Function getHistory(ByVal DocId As Long) As DataSet
         Dim ds As DataSet = Nothing
         Dim strSelect As String = "SELECT Fecha, USR.NAME Usuario, MSG_TO Para, MSG_CC CC, MSG_BCC BCC, MSG_SUBJECT Asunto, H.ID FROM ZMAIL_HISTORY H INNER JOIN USRTABLE USR ON H.USR_ZMB = USR.ID WHERE DOC_ID = " & DocId & " ORDER BY FECHA DESC"
