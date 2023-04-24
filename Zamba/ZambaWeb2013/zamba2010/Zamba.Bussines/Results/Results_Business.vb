63cc6331f5d0473d648b26ac3baa332cea7efdd7
StardocTeam <46877778+StardocTeam@users.noreply.github.com>
2023-04-24 13:13:27 -0300

    ML: Se agrega el proceso de PAI Insert, la posibilidad de replicas con associados.

--- Zamba/ZambaWeb2013/zamba2010/Zamba.Bussines/Results/Results_Business.vb
+++ Zamba/ZambaWeb2013/zamba2010/Zamba.Bussines/Results/Results_Business.vb
@@ -1450,7 +1450,7 @@ Public Class Results_Business
             newResult.Name = GetResultName(newResult)
             ZTrace.WriteLineIf(ZTrace.IsInfo, "Nombre: " & newResult.Name)
 
-            If isVirtual = False OrElse isReplica OrElse (Not String.IsNullOrEmpty(forceBlob) AndAlso Boolean.Parse(forceBlob)) Then 'Intento cargar un volumen para el documento, si no se puede se genera una excepcion
+            If isVirtual = False OrElse isReplica OrElse (isVirtual = False AndAlso Not String.IsNullOrEmpty(forceBlob) AndAlso Boolean.Parse(forceBlob)) Then 'Intento cargar un volumen para el documento, si no se puede se genera una excepcion
                 ZTrace.WriteLineIf(ZTrace.IsInfo, "cargando volumen")
                 LoadVolume(newResult)
                 ZTrace.WriteLineIf(ZTrace.IsInfo, "se cargo el volumen")
@@ -1734,7 +1734,7 @@ Public Class Results_Business
 
     Public Sub MapMail(newResult As INewResult)
         ZTrace.WriteLineIf(ZTrace.IsVerbose, "[MapMail]: Validando MSG...")
-        If newResult.FileName.ToLower().EndsWith(".msg") And File.Exists(newResult.FullPath) Then
+        If newResult.FileName.ToLower().EndsWith(".msg") AndAlso File.Exists(newResult.FullPath) Then
             ZTrace.WriteLineIf(ZTrace.IsVerbose, "[MapMail]: Mapeando archivo MSG...")
             Dim message As MailMessage = MailMessage.Load(newResult.FullPath)
 
