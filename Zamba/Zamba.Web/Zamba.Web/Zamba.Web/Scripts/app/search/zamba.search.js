77f0838482ac3696894a33822a3e2c728a31f6f7
EmilianoAlvarezST <69012148+EmilianoAlvarezST@users.noreply.github.com>
2023-09-01 17:12:51 -0300

    EA-Modoc: Ahora los resultados se pueden visualizar en los respectivos modos de visualizacion.

--- Zamba/Zamba.Web/Zamba.Web/Zamba.Web/Scripts/app/search/zamba.search.js
+++ Zamba/Zamba.Web/Zamba.Web/Zamba.Web/Scripts/app/search/zamba.search.js
@@ -5571,10 +5571,10 @@ app.controller('maincontroller', function ($scope, $attrs, $http, $compile, Enti
         var $active = $items.children(".resultsGridActive").index();
         $items.children().removeClass("resultsGridActive");
         if (event != undefined) {
-            var item = this.Search.SearchResults[event == undefined ? index - 1 : index];
+            var item = this.Search.SearchResultsObject.data[event == undefined ? index - 1 : index];
 
-            if (this.Search.SearchResults != undefined) {
-                this.Search.SearchResults = this.Search.SearchResults.map(function (itemResult) {
+            if (this.Search.SearchResultsObject.data != undefined) {
+                this.Search.SearchResultsObject.data = this.Search.SearchResultsObject.data.map(function (itemResult) {
                     itemResult.NGSelectedRow = false;
                     return itemResult;
                 });
