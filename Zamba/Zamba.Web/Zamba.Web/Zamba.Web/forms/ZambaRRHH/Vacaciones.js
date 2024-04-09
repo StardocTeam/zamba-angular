54bb985029392a46d62efe6d075d068c11d35f9c
sebastianmaioccostardoc <sebastian.maiocco@stardoc.com.ar>
2024-04-09 16:18:10 -0300

    Zamba Dashboard RRHH Vacaciones (varios)

--- Zamba/Zamba.Web/Zamba.Web/Zamba.Web/forms/ZambaRRHH/Vacaciones.js
+++ Zamba/Zamba.Web/Zamba.Web/Zamba.Web/forms/ZambaRRHH/Vacaciones.js
@@ -11,22 +11,43 @@
 //zamba_index_230 (fecha hasta opcion 2)
 //zamba_index_239 (dias solicitados opcion 2)
 //zamba_index_226 (Autorizar opcion 2)
-debugger;
-document.addEventListener("DOMContentLoaded", function () {
-    InitFormVacationsInsert();
-});
-//window.onload = function () {
+
+//document.addEventListener("DOMContentLoaded", function () {
 //    InitFormVacationsInsert();
-//}
+//});
 
 function InitFormVacationsInsert() {
-    debugger;
     SetEventsfillTotalDays();
     setEventsRequestDays();
 }
-function SetEventsfillTotalDays() {
+
+function InitFormVacationsAuthorization() {
+
+    document.getElementById('zamba_index_225').addEventListener('change', function () {
+        checkedAuthorizationDateRange(1);
+    });
+    document.getElementById('zamba_index_226').addEventListener('change', function () {
+        checkedAuthorizationDateRange(2);
+    });
+
+    //SetEventsfillTotalDays();
+    //setEventsRequestDays();
+}
+function checkedAuthorizationDateRange(option) {
+    if (option == 1) {
+        if (document.getElementById('zamba_index_225').value = "checked") {
+            document.getElementById('zamba_index_226').value = "unchecked"
+        }
+    } else {
+        if (document.getElementById('zamba_index_226').value = "checked") {
+            document.getElementById('zamba_index_225').value = "unchecked"
+        }
+    }
     
-    document.getElementById('zamba_index_166').addEventListener('blur', function () {        
+}
+function SetEventsfillTotalDays() {
+
+    document.getElementById('zamba_index_166').addEventListener('blur', function () {
         fillTotalDays();
     });
     document.getElementById('zamba_index_167').addEventListener('blur', function () {
@@ -34,7 +55,7 @@ function SetEventsfillTotalDays() {
     });
     fillTotalDays();
 }
-function setEventsRequestDays(){
+function setEventsRequestDays() {
     document.getElementById('zamba_index_227').addEventListener('blur', function () {
         fillRequestedDays();
     });
@@ -47,57 +68,99 @@ function setEventsRequestDays(){
     document.getElementById('zamba_index_230').addEventListener('blur', function () {
         fillRequestedDays();
     });
-
+    document.getElementById('zamba_index_168').addEventListener('blur', function () {
+        fillRequestedDays();
+    });
+    document.getElementById('zamba_index_239').addEventListener('blur', function () {
+        fillRequestedDays();
+    });
+    document.getElementById('zamba_index_168').addEventListener('focus', function () {
+        fillRequestedDays();
+    });
+    document.getElementById('zamba_index_239').addEventListener('focus', function () {
+        fillRequestedDays();
+    });
     fillRequestedDays();
 }
+function DateToStringDDMMYYYY(fecha) {
+    let partes = fecha.split('/');
+    let mes = partes[1];
+    let dia = partes[0];
+    let anio = partes[2];
+    let nuevaFecha = `${mes}/${dia}/${anio}`;
+    return new Date(nuevaFecha);
+}
+
 function fillRequestedDays() {
-    var chooseWorkingDays = false;
-    var Option1DateFrom = new Date(document.getElementById('zamba_index_227').value);
-    var Option1DateTo = new Date(document.getElementById('zamba_index_228').value);
-    var Option2DateFrom = new Date(document.getElementById('zamba_index_229').value);;;
-    var Option2DateTo = new Date(document.getElementById('zamba_index_230').value);;;
+    var chooseWorkingDays = true;
+    
+    var Option1DateFrom = DateToStringDDMMYYYY(document.getElementById('zamba_index_227').value);
+    var Option1DateTo = DateToStringDDMMYYYY(document.getElementById('zamba_index_228').value);
+    var Option2DateFrom = DateToStringDDMMYYYY(document.getElementById('zamba_index_229').value);
+    var Option2DateTo = DateToStringDDMMYYYY(document.getElementById('zamba_index_230').value);
+    debugger;
+
     if (!isNaN(Option1DateFrom.getTime()) && !isNaN(Option1DateTo.getTime())) {
         if (chooseWorkingDays) {
-            document.getElementById('zamba_index_168').value = calculateWorkingDays(Option1DateFrom, Option1DateTo).toString();
+            document.getElementById('zamba_index_168').value = calculateWorkingDays(Option1DateFrom, Option1DateTo,1).toString();
         }
         else {
-            document.getElementById('zamba_index_168').value = calculateContinuesDays(Option1DateFrom, Option1DateTo).toString();
-        }        
+            document.getElementById('zamba_index_168').value = calculateContinuesDays(Option1DateFrom, Option1DateTo,1).toString();
+        }
     } else {
         document.getElementById('zamba_index_168').value = "";
     }
     if (!isNaN(Option2DateFrom.getTime()) && !isNaN(Option2DateTo.getTime())) {
         if (chooseWorkingDays) {
-            document.getElementById('zamba_index_239').value = calculateWorkingDays(Option2DateFrom, Option2DateTo).toString();
+            document.getElementById('zamba_index_239').value = calculateWorkingDays(Option2DateFrom, Option2DateTo,2).toString();
         }
         else {
-            document.getElementById('zamba_index_239').value = calculateContinuesDays(Option1DateFrom, Option2DateTo).toString();
-        }        
+            document.getElementById('zamba_index_239').value = calculateContinuesDays(Option2DateFrom, Option2DateTo,2).toString();
+        }
     } else {
         document.getElementById('zamba_index_239').value = "";
     }
 }
-function calculateWorkingDays(dateFrom, dateTo) {
+function calculateWorkingDays(dateFrom, dateTo, option) {
+    document.getElementById("error_opcion_" + option).style.display = "none"
+    if (dateFrom <= dateTo) {
+        var totalDays = Math.round((dateTo - dateFrom) / (1000 * 60 * 60 * 24)) + 1;
+        var workingDays = 0;
 
-    var totalDays = Math.round((dateTo - dateFrom) / (1000 * 60 * 60 * 24)) + 1;    
-    var workingDays = 0;
-    
-    for (var i = 0; i < totalDays; i++) {
-        var day = new Date(dateFrom);
-        day.setDate(day.getDate() + i);
-        
-        if (day.getDay() !== 0 && day.getDay() !== 6) {
-            workingDays++;
+        for (var i = 0; i < totalDays; i++) {
+            var day = new Date(dateFrom);
+            day.setDate(day.getDate() + i);
+
+            if (day.getDay() !== 0 && day.getDay() !== 6) {
+                workingDays++;
+            }
         }
+        return workingDays;
     }
-    return workingDays;
+    else {
+        ShowInvalidDate(option);
+        return "";
+    }
 }
-function calculateContinuesDays(dateFrom, DateTo) {    
-    return Math.round((dateFrom - DateTo) / (1000 * 60 * 60 * 24));
+function calculateContinuesDays(dateFrom, DateTo, option) {
+    document.getElementById("error_opcion_" + option).style.display = "none"
+    if (dateFrom <= DateTo) {
+        
+        return Math.round((DateTo - dateFrom) / (1000 * 60 * 60 * 24)) + 1;
+        
+    }
+    else {
+        ShowInvalidDate(option);
+        return "";
+    }
+    
+}
+function ShowInvalidDate(option) {
+    document.getElementById("error_opcion_" + option).style.display = "block";
 }
 function fillTotalDays() {
     var pendingDays = parseFloat(document.getElementById('zamba_index_166').value);
-    var periodDays = parseFloat(document.getElementById('zamba_index_167').value);    
+    var periodDays = parseFloat(document.getElementById('zamba_index_167').value);
     if (!isNaN(pendingDays) && pendingDays >= 0 && !isNaN(periodDays) && periodDays >= 0) {
         var totalDays = pendingDays + periodDays;
         document.getElementById('zamba_index_169').value = totalDays.toString();
