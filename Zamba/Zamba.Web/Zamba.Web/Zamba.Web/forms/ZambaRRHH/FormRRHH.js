54c56ba5f4240aa06fdfd0fbbb869fc1d2b8acb5
sebastianmaioccostardoc <sebastian.maiocco@stardoc.com.ar>
2024-04-26 12:20:53 -0300

    SM: zt-416-Cambios pequeños

--- Zamba/Zamba.Web/Zamba.Web/Zamba.Web/forms/ZambaRRHH/FormRRHH.js
+++ Zamba/Zamba.Web/Zamba.Web/Zamba.Web/forms/ZambaRRHH/FormRRHH.js
@@ -8,6 +8,7 @@ function SendMessageToDashBoard(type, data, error) {
         error: error
     }
     var messageJSON = JSON.stringify(message);
+    parent.postMessage(messageJSON, scheme + "://" + domainDashboardRRHH);
 }
 function GetDomain() {
     var key = "RRHHApiServerName";
