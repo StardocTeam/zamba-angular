49f2d7c0a415181d2838f7765134d0724ecd6175
sebastianmaioccostardoc <sebastian.maiocco@stardoc.com.ar>
2024-04-29 10:11:48 -0300

    SM: zt-416 Comunicacion dashboard con zamba.wb

--- Zamba/Zamba.Web/Zamba.Web/Zamba.Web/forms/ZambaRRHH/FormRRHH.js
+++ Zamba/Zamba.Web/Zamba.Web/Zamba.Web/forms/ZambaRRHH/FormRRHH.js
@@ -17,5 +17,39 @@ function GetDomain() {
 }
 function InitFormGeneric() {
     GetDomain();
+    window.addEventListener('message', event => {
+        var url = new URL(environment['zambaWeb']);
+        var origin = scheme + "://" + domainDashboardRRHH;
+        if (event.origin != origin) {
+            return;
+        }
+        var message = JSON.parse(event.data);
+        this.handleMessage(message);
+    });
+
+}
+
+function handleMessage(message) {
+    try {
+        switch (message.type) {
+            case 'request vacation':
+                console.log(message.data);
+                break;
+        }
+    } catch (e) {
+        console.error("Error al procesar el mensaje: ", e);
+    }
 }
 InitFormGeneric();
+
+
+
+
+
+
+
+
+
+
+
+
