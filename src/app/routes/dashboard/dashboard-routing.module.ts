def694992b6478d0e7b9418cd130aae841e98212
EmilianoAlvarezST <69012148+EmilianoAlvarezST@users.noreply.github.com>
2024-06-07 17:26:49 -0300

    EA}-RRHH}-ZT397: Updated navigation route when i log in the app

--- src/app/routes/dashboard/dashboard-routing.module.ts
+++ src/app/routes/dashboard/dashboard-routing.module.ts
@@ -9,7 +9,7 @@ import { DefaultComponent } from '../default/default.component';
 import { WidgetsContainerComponent } from '../widgets-container/widgets-container.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'default', pathMatch: 'full' },
+  { path: '', redirectTo: 'widgets', pathMatch: 'full' },
   { path: 'default', component: DefaultComponent },
   { path: 'v2', component: DashboardV1Component },
   { path: 'analysis', component: DashboardAnalysisComponent },
@@ -22,4 +22,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule {}
+export class DashboardRoutingModule { }
