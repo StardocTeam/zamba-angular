c2f249ebc0c35967635217a643d0e6b92df88dd2
NicolasCabrera <nicolas.cabrera@stardoc.com.ar>
2025-10-03 17:37:49 -0300

    NC: prueba de cambio de rutas, segun el environment -nuevo env rrhh

---
+++ src/app/routes/widgets/dummy/dummy.component.ts
@@ -0,0 +1,21 @@
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { GridsterItem } from 'angular-gridster2';
+
+import { WidgetsModule } from '../widgets.module';
+/* Agregar los otros widgets
+import { WidgetAComponent } from './widgetA.component';
+import { WidgetBComponent } from './widgetB.component';
+import { WidgetCComponent } from './widgetC.component';
+*/
+@Component({
+  selector: 'dummy',
+  templateUrl: './dummy.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['dummy.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: []
+})
+export class DummyComponent { }
+
