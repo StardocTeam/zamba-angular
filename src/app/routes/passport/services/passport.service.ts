4ac05cb009c905edab0f50132c585bbe6cef92bf
NicolasCabrera <nicolas.cabrera@stardoc.com.ar>
2025-10-06 16:41:35 -0300

    NC: se vuelve a agregar el login

--- src/app/routes/passport/services/passport.service.ts
+++ src/app/routes/passport/services/passport.service.ts
@@ -37,7 +37,7 @@ export class PassportService {
           return throwError(() => error);
         }),
         tap(res => {
-          //res = JSON.parse(res);
+          res = JSON.parse(res);
 
           console.log(res);
           res.user = {
